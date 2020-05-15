b7f02db9a5574bbaec808e0610adbf60db6ddd3c
Sysm94 <65374666+Sysm94@users.noreply.github.com>
2020-05-15 12:28:16 -0300

    Update JDBCSenhaDAO.java

--- exemploAutenticacao/src/br/com/exemploAutenticacao/jdbc/JDBCSenhaDAO.java
+++ exemploAutenticacao/src/br/com/exemploAutenticacao/jdbc/JDBCSenhaDAO.java
@@ -14,8 +14,7 @@ private Connection conexao;
 		this.conexao = conexao;		
 	}
 
-public Usuario validaUsuario(Usuario usuario){
-		
+public Usuario validaUsuario(Usuario usuario){		
 				
 		String comando = "select id,usuario,senha from usuario where usuario='"+usuario.getUsuario()+"' and senha='"+usuario.getSenha()+"'";
 		Usuario login = null;
@@ -26,16 +25,16 @@ public Usuario validaUsuario(Usuario usuario){
 		
 		
 		if(rs.next()){
-			login= new Usuario();
-			
-			login.setId(rs.getInt("id"));
+			login= new Usuario();			
+			login.setId(rs.getInt("id"));		
+			login.setUsuario(rs.getString("usuario"));
+			login.setSenha(rs.getString("senha"));
 		}
 			
 		}catch (SQLException e){
 			e.printStackTrace();			
-			
-			
-		}		
+		}	
+	
 		return login;	
 	}
 }
